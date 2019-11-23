5a4dd6a4d6c460feb3fc581d613348f9e864fefe
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-23 00:51:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,7 @@ def assign_rooms(room)
 end
 
 def printer(name) # names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  batch_badge_creator(name).each do |a|
+  batch_badge_creator(name).each do |a| # ["Hello, my name is #{name}."]
     puts a 
   end 
   assign_rooms(name).each do |b|
